5f2eb618a7e466d1cda9fea01602cc8c52762a8f
yuanpeng01 <yuanpeng.3007@163.com>
2022-07-06 17:33:06 +0800

    add zip

--- get-icon.py
+++ get-icon.py
@@ -1,18 +1,44 @@
 
 import json
 import re
+from urllib.parse import urlparse
 import requests
+import os.path
+import os
+
+HOST = "https://ted-developer.github.io/game-files"
+LOCAL_WWW_ROOT = "/Users/yuanpeng01/Documents/code/game-cdn/cache"
+LOCAL = os.path.dirname(__file__)
 
 f = open("/Users/yuanpeng01/Downloads/download.json", "r")
 list = json.loads(f.read())
+
 
 for item in list:
     fileName = item['name']
     fileUrl = item['image_url']
     fielExt = re.search(".[^\.]+$", fileUrl).group(0)
 
-    r = requests.get(fileUrl)
+    # save icon image
+    filePath = LOCAL + "/images/icons/" + fileName + fielExt
+    if not os.path.exists(filePath):
+        r = requests.get(fileUrl)
+        img = open(filePath, 'wb')
+        img.write(r.content)
+        img.close()
 
-    img = open("images/icons/" + fileName + fielExt, 'wb')
-    img.write(r.content)
-    img.close()
+    item['image_url'] = HOST + "/" + filePath
+
+    # save game files
+    urlPath = urlparse(item['url'])
+    srcPath = os.path.dirname(LOCAL_WWW_ROOT + urlPath.path)
+    filePath = LOCAL + "/pack/" + fileName + ".7z"
+
+    print(srcPath)
+    if not os.path.exists(filePath):
+        # 压缩
+        os.system("7z a "+filePath+" "+srcPath)
+
+# save game list
+gameList = open(LOCAL + "/game-list.json", 'w')
+json.dump(list, gameList)
