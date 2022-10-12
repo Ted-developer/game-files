27a3a0009a82f5e74ff2284416bef4756235a967
xiaodugame <xiaodugame@xiaodugamedeMacBook-Pro.local>
2022-10-12 20:56:59 +0800

    add files

--- get-icon.py
+++ get-icon.py
@@ -13,10 +13,32 @@ HOST = "https://ted-developer.github.io/game-files"
 LOCAL_WWW_ROOT = "/Users/xiaodugame/Documents/work/code/get-game/cache"
 LOCAL = os.path.dirname(__file__)
 key = [9]
+DOWNLOAD_PATH = "/Users/xiaodugame/Downloads"
 
-f = open("/Users/xiaodugame/Downloads/download.json", "r")
-list = json.loads(f.read())
+# merge 多个download.json，去重
+downloadData = []
+# 读取download文件
+for subdir, dirs, files in os.walk(DOWNLOAD_PATH):
+    for file_name in files:
+        if file_name.startswith("download."):
+            print(os.path.join(subdir, file_name))
+            f = open(os.path.join(subdir, file_name), "r")
+            downloadData += json.loads(f.read())
 
+def mergeList(src, data):
+    if len(src):
+        nameMap = dict(zip([item["name"] for item in src], cycle([1])))
+    else:
+        nameMap = {}
+
+    for item in data:
+        if nameMap.get(item["name"]) == 1:
+            continue
+        
+        src += [item]
+        nameMap[item["name"]] = 1
+
+    return src
 
 def encryteFile(name):
     file = open(name, "r")
@@ -28,6 +50,8 @@ def encryteFile(name):
     fw = open(name, "wb")
     fw.write(bytes(encrypted))
 
+
+list = mergeList([], downloadData)
 
 for item in list:
     fileName = item['name']
@@ -70,6 +94,10 @@ for item in list:
         # 压缩
         #os.system("7z a "+filePath+" "+srcPath)
         os.system("7z a "+filePath+" /tmp/"+fileName)
+
+# merge new download list with history 
+gameList = open(LOCAL + "/game-list.json", 'r')
+list = mergeList(json.loads(gameList.read())['game_list'], list)
 
 # save game list
 gameList = open(LOCAL + "/game-list.json", 'w')
